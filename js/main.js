d094c881ce66a37558cf7194c508583066ecc75c
niminskiy <s.niminskiy@gmail.com>
2020-06-06 01:26:09 +0300

    Отправляем домашку в новую ветку

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
 
-let income = 10; 
-let addExpenses = 20; 
-let deposit = 30; 
-let mission = 40; 
-let period = 4;
 
+// ------------Lesson 2-------------
 
-alert ('Hello!');
+// 1). Присваиваем значения переменным:
 
-console.log('income =', income);
+let money = 10000;
+let income = 'фриланс';
+let addExpenses = 'коммуналка, автомобиль, кредит';
+let deposit = true;
+let mission = 600000;
+let period = 12;
+
+// 2). Выводим в консль значения:
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+    // --- -работаем с addExpenses
+
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+    // --- -работаем с переменной budgetDay
+
+let budgetDay = money;
+money /= 30;
+console.log('budgetDay: ', money);
+
+// 3). Проверено! Ошибок в консоли нет.
+
+// 4). Добавляем ветку lesson2 на GitHub.
