22df865a6f74f266a5eb39d488fee73aa972f10a
niminskiy <s.niminskiy@gmail.com>
2020-06-08 22:19:43 +0300

    Создал ветку lesson3

--- js/main.js
+++ js/main.js
@@ -1,11 +1,49 @@
+// ------------Lesson 3-------------
 
-let income = 10; 
-let addExpenses = 20; 
-let deposit = 30; 
-let mission = 40; 
-let period = 4;
+// 1). Присваиваем значения переменным:
 
+let money = 10000;
+console.log(money);
+let income = 'фриланс';
+console.log(income);
+let addExpenses = 'Коммуналка, Автомобиль, Кредит, Медицина, Обучение';
+console.log(addExpenses);
+let deposit = true;
+console.log(deposit);
+let mission = 600000;
+console.log(mission);
+let period = 12;
+console.log(period);
 
-alert ('Hello!');
+// 2). Выводим в консль тип переменных:
 
-console.log('income =', income);
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+    // --- -работаем с addExpenses
+
+console.log(addExpenses.length);
+
+    // --- привел переменную addExpenses к нижнему регистру
+
+// let lower = addExpenses. toLowerCase();
+// addExpenses = lower;
+// console.log(addExpenses);
+
+addExpenses = addExpenses. toLowerCase();
+console.log(addExpenses);
+
+    // --- вывел массив в консоль
+
+console.log(addExpenses.split(', '));
+console.log(addExpenses);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+    // --- -работаем с переменной budgetDay
+
+let budgetDay = money;
+money /= 30;
+console.log('budgetDay: ', money);
