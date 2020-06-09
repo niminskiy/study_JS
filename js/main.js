5f642ca0fa9eaac3abbc46788e0dc388f98cb223
niminskiy <s.niminskiy@gmail.com>
2020-06-09 21:02:22 +0300

    Update main.js
    End

--- js/main.js
+++ js/main.js
@@ -78,15 +78,11 @@ let budgetDay = budgetMonth / 30;
 console.log('Ваш бюджет на день: ' +Math.floor(budgetDay)+ 'р'); //округлить в меньшую сторону Math.floor(число)
 
 if (budgetDay >= 1200){
-    console.log('У вас высокий уровень дохода')
+    console.log('У вас высокий уровень дохода');
   } else if (budgetDay >=600){
-    console.log('У вас средний уровень дохода')
+    console.log('У вас средний уровень дохода');
   } else if (budgetDay >= 0){
-    console.log('К сожалению у вас уровень дохода ниже среднего”')
+    console.log('К сожалению у вас уровень дохода ниже среднего”');
   } else {
-    console.log('Что то пошло не так')
+    console.log('Что то пошло не так');
   };
-
-
-
-
