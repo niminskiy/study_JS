0f65cabc218f8d0e0063c32d3a62353e07d89073
niminskiy <s.niminskiy@gmail.com>
2020-06-09 20:45:14 +0300

    Update main.js
    Готово!

--- js/main.js
+++ js/main.js
@@ -3,47 +3,47 @@
 
 // 1). Присваиваем значения переменным:
 
-// let money = 10000;  //доход за месяц
-// console.log(money);
-// let income = 'фриланс';  //дополнительныый доход
-// console.log(income);
-// let addExpenses = 'Коммуналка, Автомобиль, Кредит, Медицина, Обучение'; //дополнительные расходы
-// console.log(addExpenses);
-// let deposit = true; //булевое
-// console.log(deposit);
-// let mission = 600000; //какую сумму хотим накопить
-// console.log(mission);
-// let period = 12; //период накопления (в месяцах)
-// console.log(period);
+/*let money = 10000;  //доход за месяц
+console.log(money);
+let income = 'фриланс';  //дополнительныый доход
+console.log(income);
+let addExpenses = 'Коммуналка, Автомобиль, Кредит, Медицина, Обучение'; //дополнительные расходы
+console.log(addExpenses);
+let deposit = true; //булевое
+console.log(deposit);
+let mission = 600000; //какую сумму хотим накопить
+console.log(mission);
+let period = 12; //период накопления (в месяцах)
+console.log(period);
 
 // 2). Выводим в консль тип переменных:
 
-// console.log(typeof money);
-// console.log(typeof income);
-// console.log(typeof deposit);
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
 // --- -работаем с addExpenses
 
-// console.log(addExpenses.length);
+console.log(addExpenses.length);
 
 // --- привел переменную addExpenses к нижнему регистру
 
-// addExpenses = addExpenses. toLowerCase();
-// console.log(addExpenses);
+addExpenses = addExpenses. toLowerCase();
+console.log(addExpenses);
 
 // --- вывел массив в консоль
 
-// console.log(addExpenses.split(', '));
-// console.log(addExpenses);
+console.log(addExpenses.split(', '));
+console.log(addExpenses);
 
-// console.log('Период равен ' + period + ' месяцев');
-// console.log('Цель заработать ' + mission + ' рублей');
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
 
 // --- -работаем с переменной budgetDay
 
-// let budgetDay = money;
-// money /= 30;
-// console.log('budgetDay: ', money);
+let budgetDay = money;
+money /= 30;
+console.log('budgetDay: ', money);*/
 
 
 
@@ -72,13 +72,6 @@ let amount2 = prompt('Во сколько вам обойдется врорая
 let budgetMonth = money - amount1 - amount2;
 console.log('Ваш бюджет на месяц: ' +budgetMonth+ 'р');
 
-let budgetDay = budgetMonth / 30;
-console.log('Ваш бюджет на день: ' +budgetDay+ 'р');
-
-
-let period = mission / budgetMonth;
-console.log('Цель будет достигнута за: ' + period + ' месяцев');
-
 console.log('Цель будет достигнута за: ' +Math.ceil(period)+ ' месяцев'); //округлить в большую сторону Math.ceil(число)
 
 let budgetDay = budgetMonth / 30;
@@ -88,9 +81,9 @@ if (budgetDay >= 1200){
     console.log('У вас высокий уровень дохода')
   } else if (budgetDay >=600){
     console.log('У вас средний уровень дохода')
-  } else if (budgetDay < 600){
+  } else if (budgetDay >= 0){
     console.log('К сожалению у вас уровень дохода ниже среднего”')
-  } else if (budgetDay <= 0){
+  } else {
     console.log('Что то пошло не так')
   };
 
