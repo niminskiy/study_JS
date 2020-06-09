35f240dddf6143f9a42c5a3dcf34e902b4a8b451
niminskiy <s.niminskiy@gmail.com>
2020-06-09 08:34:21 +0300

    Дошел до округления значений

--- js/main.js
+++ js/main.js
@@ -1,90 +1,46 @@
 // ------------Lesson 3-------------
 'use strict'
 
-// 1). Присваиваем значения переменным:
 
-// let money = 10000;  //доход за месяц
-// console.log(money);
+
 // let income = 'фриланс';  //дополнительныый доход
-// console.log(income);
+
 // let addExpenses = 'Коммуналка, Автомобиль, Кредит, Медицина, Обучение'; //дополнительные расходы
-// console.log(addExpenses);
+
 // let deposit = true; //булевое
-// console.log(deposit);
-// let mission = 600000; //какую сумму хотим накопить
-// console.log(mission);
-// let period = 12; //период накопления (в месяцах)
-// console.log(period);
 
 
-// 2). Выводим в консль тип переменных:
-
-// console.log(typeof money);
-// console.log(typeof income);
-// console.log(typeof deposit);
-
-    // --- -работаем с addExpenses
-
-// console.log(addExpenses.length);
-
-    // --- привел переменную addExpenses к нижнему регистру
-
-// let lower = addExpenses. toLowerCase();
-// addExpenses = lower;
-// console.log(addExpenses);
-
-// addExpenses = addExpenses. toLowerCase();
-// console.log(addExpenses);
-
-    // --- вывел массив в консоль
-
-// console.log(addExpenses.split(', '));
-// console.log(addExpenses);
-
-// console.log('Период равен ' + period + ' месяцев');
-// console.log('Цель заработать ' + mission + ' рублей');
-
-    // --- -работаем с переменной budgetDay
-
-// let budgetDay = money;
-// money /= 30;
-// console.log('budgetDay: ', money);
 
 // -----------Начало 3-го задания-------------
 
 
+let mission = +prompt('Сколько вы хотите накопить денег?');
+console.log('Цель накопить ' +mission+ ' рублей');
 
-let money = prompt('Ваш месячный доход?');
-// console.log(money);
-// console.log(typeof money);
+let money = +prompt('Какой у вас месячный доход?');
+console.log('Месячный доход: ' +money+ ' рублей');
 
-// let addExpenses = prompt('Пересислите возможные расходы за месяц через запятую');
-// console.log(addExpenses);
+let addExpenses = prompt('Пересислите возможные расходы за месяц через запятую');
+console.log(addExpenses.split(', '));
 
-// let deposit = prompt('Есть ли у вас депозит в банке?');
-// console.log(deposit);
+let deposit = prompt('Есть ли у вас депозит в банке?');
+console.log(deposit);
 
-// let expenses1 = prompt('Введите обязательную статью расходов 1');
-// console.log(expenses1);
+let expenses1 = prompt('Введите обязательную статью расходов', 'текст');
+let amount1 = prompt('Во сколько вам обойдется первая статья?', 'сумма');
 
-// let expenses2 = prompt('Введите обязательную статью расходов 2');
-// console.log(expenses2);
-
-let amount1 = prompt('Во сколько вам обойдется первая статья?');
-// console.log(amount1);
-
-let amount2 = prompt('Во сколько вам обойдется врорая статья?');
-// console.log(amount2);
+let expenses2 = prompt('Введите обязательную статью расходов', 'текст');
+let amount2 = prompt('Во сколько вам обойдется врорая статья?', 'сумма');
 
 
 let budgetMonth = money - amount1 - amount2;
-console.log('Бюджет на месяц ' +budgetMonth+ 'р');
+console.log('Ваш бюджет на месяц: ' +budgetMonth+ 'р');
 
-let budgetDay = budgetMonth / 12;
-console.log('Бюджет на день ' +budgetDay+);
+let budgetDay = budgetMonth / 30;
+console.log('Ваш бюджет на день: ' +budgetDay+ 'р');
 
 
-
-// console.log('Цель будет достигнута за ' + period + ' месяцев');
+let period = mission / budgetMonth;
+console.log('Цель будет достигнута за: ' + period + ' месяцев');
 
 // console.log('У вас высокий уровень дохода');
