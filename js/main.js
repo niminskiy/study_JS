007ac528055dc04120feb27c4693b16ca0eec9a8
niminskiy <s.niminskiy@gmail.com>
2020-06-11 00:30:09 +0300

    Update main.js
    Больше вопросов, чем ответов.

--- js/main.js
+++ js/main.js
@@ -2,64 +2,68 @@
 
 
 let mission = +prompt('Сколько вы хотите накопить денег?');
-console.log('Цель накопить ' +mission+ ' рублей');
+//console.log('Цель накопить ' +mission+ ' рублей');
 
 let money = +prompt('Какой у вас месячный доход?');
-console.log('Месячный доход: ' +money+ ' рублей');
+//console.log('Месячный доход: ' +money+ ' рублей');
 
-//let addExpenses = prompt('Пересислите возможные расходы за месяц через запятую');
-//console.log(addExpenses.split(', '));
+let addExpenses = prompt('Пересислите возможные расходы за месяц через запятую');
+console.log(addExpenses.split(', '));
 
-//let deposit = prompt('Есть ли у вас депозит в банке?');
-//console.log(deposit);
+let deposit = prompt('Есть ли у вас депозит в банке?');
 
-//let expenses1 = prompt('Введите обязательную статью расходов', 'текст');
+
+let expenses1 = prompt('Введите обязательную статью расходов', 'текст');
 let amount1 = prompt('Во сколько вам обойдется первая статья?', 'сумма');
 
-//let expenses2 = prompt('Введите обязательную статью расходов', 'текст');
+
+let expenses2 = prompt('Введите обязательную статью расходов', 'текст');
 let amount2 = prompt('Во сколько вам обойдется врорая статья?', 'сумма');
 
 
-//let budgetMonth = money - amount1 - amount2;
-//console.log('Ваш бюджет на месяц: ' +budgetMonth+ 'р');
+// let budgetMonth = money - amount1 - amount2;
+// console.log('Ваш бюджет на месяц: ' +budgetMonth+ 'р');
 
-//let period = mission / budgetMonth;
-//console.log('Цель будет достигнута за: ' +Math.ceil(period)+ ' месяцев');
 
-//let budgetDay = budgetMonth / 30;
-//console.log('Ваш бюджет на день: ' +Math.floor(budgetDay)+ 'р');
+let period = mission / accumulatedMonth;
+console.log('Цель будет достигнута за: ' +Math.ceil(period)+ ' месяцев');
 
-// if (budgetDay >= 1200){
-//   console.log('У вас высокий уровень дохода');
-// } else if (budgetDay >=600){
-//   console.log('У вас средний уровень дохода');
-// } else if (budgetDay < 600){
-//   console.log('К сожалению у вас уровень дохода ниже среднего”');
-// } else if (budgetDay <= 0){
-//   console.log('Что то пошло не так');
-// }
+
+let budgetDay = accumulatedMonth / 30;
+console.log('Ваш бюджет на день: ' +Math.floor(budgetDay)+ 'р');
+
+
+if (budgetDay >= 1200){
+  console.log('У вас высокий уровень дохода');
+} else if (budgetDay >=600){
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay < 600){
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay <= 0){
+  console.log('Что то пошло не так');
+}
 
 
 function getExpensesMonth(){
-  return amount1 + amount2;
+
 }
-getExpensesMonth();
-console.log(getExpensesMonth);
+console.log(getExpensesMonth());
 
 
 function getAccumulatedMonth(){
-  return money - getExpensesMonth;
+  
 }
-getAccumulatedMonth();
-console.log(getAccumulatedMonth);
 
-let accumulatedMonth = getAccumulatedMonth;
+
+let accumulatedMonth = getAccumulatedMonth();
+
 
 function getTargetMonth(){
-  return mission / accumulatedMonth;
+  
 }
-getTargetMonth();
-console.log(getTargetMonth);
+console.log(getTargetMonth())
+
+
 
 
 /*1) Объявить функцию getExpensesMonth. Функция возвращает сумму всех обязательных расходов за месяц
