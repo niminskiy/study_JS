38ddbcd80788caec6594b946251376288c5cfc1e
niminskiy <s.niminskiy@gmail.com>
2020-06-15 09:19:01 +0300

    Update main.js
    ...

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ let isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-
 let money;
 
 let start = function() {
@@ -15,78 +14,93 @@ let start = function() {
   }
 };
 start();
-console.log('Ваш месячный доход ' +money+ ' рублей');
 
 
 let appData = {
   income: {},
   addIncome: [],
-  expenses: {},
-  addExpenses: [],
+  expenses: {}, // основные расходы
+  addExpenses: [], // дополнительные расходы
   deposit: false,
-  mission: 50000,
   period: 3,
+  mission: function() {
+    prompt('Сколько вы хотите накопить денег?'); 
+    console.log('Цель накопить ' +appData.mission+ ' рублей');
+  },
   asking: function() {
     let addExpenses = prompt('Пересислите возможные расходы через запятую');
-        appData.addExpenses = addExpenses.toLowerCase().split(', ');
-        appData.deposit = confirm('Есть ли у вас депозит в банке?');
-  },
+      appData.addExpenses = addExpenses.toLowerCase().split(', ');
+      appData.deposit = confirm('Есть ли у вас депозит в банке?');
 
-  budget: money,
-  budgetMonth: 0,
-  expensesMonth: 0,
-  getExpensesMonth: function() {
-    let expenses1,
-        expenses2;
-
-    let sum = 0;
-  
     for (let i = 0; i < 2; i++) {
-  
-        if (i === 0) {
-          expenses1 = prompt('Введите обязательную статью расходов');
-        } else if (i === 1) {
-          expenses2 = prompt('Введите обязательную статью расходов');
-        }
-  
-      sum += +prompt('Во сколько это обойдется');
+      
+      let tempText = prompt('Введите обязательную статью расходов');
+      let tempMoney = +prompt('Во сколько это обойдется');
+      appData.expenses[tempText] = tempMoney;
+
     }
-    return sum;
+
+  },
+  getExpensesMonth: function() {
+    
+    let expensesMonth = 0;
+    for (let key in appData.expenses) {
+      appData.expensesMonth += appData.expenses[key];
+    }
+    console.log ('Расходы на месяц: ' + expensesMonth + ' рублей');
   },
 
-  getAccumulatedMonth: function() {
-    return money - appData.expensesAmount;
-  },
+  getBudget: function() {
+    
+    appData.budgetMonth = money - appData.expensesMonth;
+    console.log(appData.bugetMonth);
   
-  getTargetMonth: function() {
-    return appData.mission / appData.getAccumulatedMonth();
+    appData.budgetDay = appData.expensesMonth / 30;
+    console.log(appData.budgetDay);
+      
   },
-  
+
   getStatusIncome: function() {
 
-    let budgetDay = appData.getAccumulatedMonth() / 30;
 
-    if (budgetDay >= 1200) {
+    if (appData.budgetDay >= 1200) {
       console.log('У вас высокий уровень дохода');
-    } else if (budgetDay >=600) {
+    } else if (appData.budgetDay >=600) {
       console.log('У вас средний уровень дохода');
-    } else if (budgetDay < 600) {
+    } else if (appData.budgetDay < 600) {
       console.log('К сожалению у вас уровень дохода ниже среднего');
-    } else if (budgetDay <= 0) {
+    } else if (appData.budgetDay <= 0) {
       console.log('Что то пошло не так');
     }
-  }
+  },
+  
 };
 appData.asking();
 
-let mission = prompt('Сколько вы хотите накопить денег?');
-console.log('Цель накопить ' +mission+ ' рублей');
+appData.getExpensesMonth();
 
-appData.expensesAmount = appData.getExpensesMonth();
-console.log ('Расходы на месяц: ' + appData.expensesAmount);
-
-appData.period = mission / appData.getAccumulatedMonth();
-console.log('Цель будет достигнута за: ' +Math.ceil(appData.period)+ ' месяцев');
+appData.getBudget();
 
 appData.getStatusIncome();
 
+//console.log('Цель будет достигнута за: ' +Math.ceil(appData.period)+ ' месяцев');
+
+
+
+for (let key in appData) {
+console.log('Наша программа включает в себя данные:  ' + key + ': ' + appData[key]);
+}
+
+/*9) getAccumulatedMonth переименовать в getBudget. Этот метод будет высчитывать значения свойств budgetMonth и budgetDay, чтобы вычислить значения используем только свойства объекта (никаких внешних переменных)
+
+10) В методах getTargetMonth и getStatusIncome исправить переменные, все значения получаем от нашего объекта appData
+
+11) Вызвать все необходимые методы после объекта, чтобы корректно считались все данные (порядок очень важен).
+
+12) В консоль вывести: 
+
+  — Расходы за месяц
+  — За какой период будет достигнута цель (в месяцах)
+  — Уровень дохода
+
+13) Используя цикл for in для объекта (appData), вывести в консоль сообщение "Наша программа включает в себя данные: " (вывести все свойства и значения)*/
