35aa6946d94b394b0d6b4b1fd7810d6724865077
niminskiy <s.niminskiy@gmail.com>
2020-06-15 16:06:13 +0300

    Update main.js
    I did it!!!

--- js/main.js
+++ js/main.js
@@ -38,9 +38,14 @@ let appData = {
     for (let i = 0; i < 2; i++) {
       
       let tempText = prompt('Введите обязательную статью расходов');
-      let tempMoney = +prompt('Во сколько это обойдется');
+
+      let tempMoney;
+
+      while (!isNumber(tempMoney)) {
+        tempMoney = +prompt('Во сколько это обойдется');
+      }
+
       appData.expenses[tempText] = tempMoney;
-      console.log(appData.expenses);
     }
 
   },
@@ -52,19 +57,23 @@ let appData = {
     console.log ('Расходы на месяц: ' + appData.expensesMonth + ' рублей');
 
   },
+  getTargetMonth: function() {
+
+    appData.period = appData.period;
+    console.log('Цель будет достигнута за: ' +Math.ceil(appData.period)+ ' месяцев');
+
+  },
 
   getBudget: function() {
     
     appData.budgetMonth = money - appData.expensesMonth;
-    console.log(appData.budgetMonth);
   
-    appData.budgetDay = appData.budgetMonth / 30;
-    console.log(appData.budgetDay);
+    appData.budgetDay = +Math.floor(appData.budgetMonth / 30);
       
   },
 
   getStatusIncome: function() {
-
+    
 
     if (appData.budgetDay >= 1200) {
       console.log('У вас высокий уровень дохода');
@@ -84,24 +93,10 @@ appData.getExpensesMonth();
 
 appData.getBudget();
 
+appData.getTargetMonth();
+
 appData.getStatusIncome();
 
-//console.log('Цель будет достигнута за: ' +Math.ceil(appData.period)+ ' месяцев');
-
-// for (let key in appData) {
-// console.log('Наша программа включает в себя данные:  ' + key + ': ' + appData[key]);
-// }
-
-/*9) getAccumulatedMonth переименовать в getBudget. Этот метод будет высчитывать значения свойств budgetMonth и budgetDay, чтобы вычислить значения используем только свойства объекта (никаких внешних переменных)
-
-10) В методах getTargetMonth и getStatusIncome исправить переменные, все значения получаем от нашего объекта appData
-
-11) Вызвать все необходимые методы после объекта, чтобы корректно считались все данные (порядок очень важен).
-
-12) В консоль вывести: 
-
-  — Расходы за месяц
-  — За какой период будет достигнута цель (в месяцах)
-  — Уровень дохода
-
-13) Используя цикл for in для объекта (appData), вывести в консоль сообщение "Наша программа включает в себя данные: " (вывести все свойства и значения)*/
+for (let key in appData) {
+console.log('Наша программа включает в себя данные:  ' + key + ': ' + appData[key]);
+}
