49e6327991e66ea839cf5c80a2b937c826630d60
niminskiy <s.niminskiy@gmail.com>
2020-06-20 18:40:38 +0300

    Update main.js
    пока только в столбик

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let isNumber = function(num) {
+/*let isNumber = function(num) {
   return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
@@ -132,19 +132,23 @@ appData.getExpensesMonth();
 //appData.getBudget();
 //appData.getTargetMonth();
 appData.getStatusIncome();
-appData.getInfoDeposit();
+appData.getInfoDeposit();*/
 
 
 //console.log('Цель будет достигнута за: ' + Math.ceil(appData.period) + ' месяцев');
-
-
 
 
 // for (let key in appData) {
 // console.log('Наша программа включает в себя данные:  ' + key + ' - ' + appData[key]);
 // }
 
+const arr = ['мама', 'папа', 'брат', 'сестра', 'сын', 'дочь'];
 
+function bigFirst(string) {
+  return string[0].toUpperCase() + string.slice(1);
+}
+
+arr.forEach(item => console.log(bigFirst(item)));
 
 
 
@@ -152,44 +156,21 @@ appData.getInfoDeposit();
 //   console.log('Расходы на месяц: ' + key + ' - ' + appData.expenses[key]);
 // }
 
-/*console.log(appData.expenses);
 
 //console.log('Пример: ' + appData.addExpenses);
 
-appData.addExpenses.forEach(function(item) {
-  //return item[0].toUpperCase() + item.slice(1);
-  appData.addExpenses.push(item[0].toUpperCase() + item.slice(1));
-});
-console.log(appData.addExpenses);
-console.log('Пример: ' + appData.addExpenses.join(', '));*/
-
-// const myArr = ['первый', 'второй', 'третий'];
-
-// appData.addExpenses.forEach(item => console.log(ucFirst('Пример: ' + appData.addExpenses.join(', '))));
-
-// function ucFirst(string) {
-//     return string.charAt(0).toUpperCase() + string.slice(1);
-// }
-
-
-// let myArr = ['первый', 'второй'];
-
-// myArr.map(item => {
-//  item = item.toString().charAt(0).toUpperCase() + item.slice(1);
-// })
-
-let words = ['первый', 'второй', 'третий'];
-for (let word of appData.addExpenses) {
-    word = word.charAt(0).toUpperCase() + word.substr(1);
-    console.log(word);
-}
-
+/*appData.addExpenses.forEach(function(item) {
+  item[0].toUpperCase() + item.slice(1);
+  console.log(appData.addExpenses.join(', '));
+  //appData.addExpenses.push(item[0].toUpperCase() + item.slice(1));
+  //console.log(appData.addExpenses);
+  //console.log('Пример: ' + appData.addExpenses.join(', '));
+});*/
 
 
 // function ucFirst(str) {
 //   return str[0].toUpperCase() + str.slice(1);
 // }
-
 // console.log(ucFirst('вася'));
 
 // for (let i = 0; i < appData.addExpenses.length; i++) {
